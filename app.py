f6cda23e32da324e525ac71b84c4cc6ed21d5840
Rubbychan-cuti <ledao261119@gmail.com>
2026-06-16 12:27:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+# Hiển thị logo
+st.image("z7921870790296_de116c7956d7f8ee5c6673013ccf709f", width=150)
+
 # Tiêu đề ứng dụng
 st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_Khánh Linh")
 
